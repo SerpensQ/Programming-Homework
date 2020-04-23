518f4654096756e2d99e48308697c9b1a4e780f3
SerpensQ <serpensq@mail.ru>
2020-04-23 23:15:53 +0500

    Финальные изменения в задании.

--- HospitalUnitTests/UnitTests.cs
+++ HospitalUnitTests/UnitTests.cs
@@ -13,10 +13,11 @@ namespace HospitalUnitTests
         public void ConstructorTestMethod()
         {
             var yoongi = CreateTestPerson();
-            
+
             Assert.AreEqual("Yoongi", yoongi.Name);
             Assert.AreEqual("Min", yoongi.Surname);
-            Assert.AreEqual("19930309", yoongi.PolicyNumber);      
+            Assert.AreEqual("19930309", yoongi.PolicyNumber);
+
 
         }
         [TestMethod]
@@ -33,13 +34,18 @@ namespace HospitalUnitTests
         {
             var yoongi = CreateTestPerson();
 
+            var youngjae = new Patient("Youngjae", "Choi", "19960917")
+            { ArrivalDate = new DateTime(2017, 07, 12), DepartureDate = new DateTime(2017, 07, 29), Service = TypeOfService.SelfPaid, CostOfTreatment = 5850 };
             var consoleOut = new[]
             {
                 "Yoongi Min",
                 $"Policy number: 19930309, The date of arrival: 20.11.2017, " +
-                $"The date of departure: 30.11.2017, Type of service: insurance covered, The cost of treatment: 4500"
+                $"The date of departure: 30.11.2017, Type of service: insurance covered, The cost of treatment: 4500",
+               "Youngjae Choi",
+                $"Policy number: 19960917, The date of arrival: 12.07.2017, " +
+                $"The date of departure: 29.07.2017, Type of service: self paid, The cost of treatment: 5850"
 
-            };
+        };
 
             TextWriter oldOut = Console.Out;
 
@@ -49,7 +55,7 @@ namespace HospitalUnitTests
                 {
                     Console.SetOut(writer);
                     yoongi.PrintInfo();
-                   
+
                 }
                 var i = 0;
 
@@ -63,11 +69,11 @@ namespace HospitalUnitTests
         }
 
         private Patient CreateTestPerson()
-        {          
-            return new Patient("Yoongi", "Min", "19930309");        
+        {
+            return new Patient("Yoongi", "Min", "19930309")
+            { ArrivalDate = new DateTime(2017, 11, 20), DepartureDate = new DateTime(2017, 11, 30), Service = TypeOfService.InsuranceCover, CostOfTreatment = 4500 };
         }
     }
 
 
-    
 }
