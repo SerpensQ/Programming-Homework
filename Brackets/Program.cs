322c42ffb2357f86ce7bcea1cbcd091cbc6115dc
SerpensQ <serpensq@mail.ru>
2020-04-24 12:27:49 +0500

    Работа на занятии.

---
+++ Brackets/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brackets
+{
+    class Program
+    {
+
+        static Dictionary<char, char> dict;
+        static void Main()
+        {
+            Console.WriteLine("Ведите пары скобок (открывающая, закрывающая) в одной строке");
+            var brackets = Console.ReadLine();
+            if (brackets.Length % 2 != 0)
+            {
+                Console.WriteLine("Скобки должны вводиться парами");
+                Console.ReadKey();
+                return;
+            }
+            for (var i = 0; i < brackets.Length; i += 2)
+                dict[brackets[i + 1]] = brackets[i];
+
+            while (true)
+            {
+                Console.WriteLine("Введите выражение со скобками(Enter-выход)");
+                var line = Console.ReadLine();
+                     if (line == "")
+                    break;
+
+
+                if (CheckBrackets(line))
+                    Console.WriteLine("Скобки расставлены верно\n");
+                else
+                    Console.WriteLine("Скобки расставлены не верно\n");
+                
+            }
+        }
+
+        static bool CheckBrackets(string expression)
+        {
+            var stack = new Stack<char>();
+
+            for(var i=0; i<expression.Length; i++)
+            {
+                var ch = expression[i];
+
+                if (dict.ContainsValue(ch))
+                    stack.Push(ch);
+
+
+                else if (dict.ContainsKey(ch))
+                    if (stack.Count == 0 || stack.Pop() != dict[ch])
+                        return false;                
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите количество знаков числа");
+            var k = int.Parse(Console.ReadLine());
+
+            var minNumber = (int)Math.Pow(10, k - 1);
+            var maxNumber = (int)Math.Pow(10, k);
+
+            Console.WriteLine("Введите количество чисел");
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+
+            var numbers = new Queue<int>[10];
+
+            for (var i = 0; i < numbers.Length; i++)
+                numbers[i] = new Queue<int>();
+
+            var rnd = new Random();
+
+            for(var i=0; i<n; i++)
+            {
+                int m = rnd.Next(minNumber, maxNumber);
+                Console.Write($"{m} ");
+                numbers[m % 10].Enqueue(m);
+            }
+
+            Console.WriteLine("\n");
+
+            for(var i=0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Числа, заканчивающиеся на {i}");
+                while (numbers[i].Count > 0)
+                    Console.Write($"{numbers[i].Dequeue()} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
