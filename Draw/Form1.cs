3ee38b2fc83cf0112f2a316cac5949aa8adbf87f
SerpensQ <serpensq@mail.ru>
2020-05-08 13:29:13 +0500

    Рисование

---
+++ Draw/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Draw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            SetStyle(ControlStyles.DoubleBuffer, true);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+      
+            using (var pen = new Pen(Color.Aquamarine, 10))
+            {
+                g.DrawLine(pen, 5, 5, 200, 200);
+                g.DrawRectangle(pen, 5, 5, 300, 200);
+            }
+          
+            g.FillEllipse(Brushes.BlueViolet, ClientSize.Width/2-50, ClientSize.Height/2-50, 100,100);
+
+            using(var pen= new Pen(Color.Sienna, 6))
+            {
+                g.DrawEllipse(pen, 3, 3, ClientSize.Width - 6, ClientSize.Height - 6);
+            }
+        }
+    }
+}
