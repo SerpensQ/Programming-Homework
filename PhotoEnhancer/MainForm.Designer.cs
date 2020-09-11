7015a1522eb2fe028664c4f951077be769daf436
SerpensQ <serpensq@mail.ru>
2020-09-11 15:58:56 +0500

    PhotoEnhancer 11.09.2020

---
+++ PhotoEnhancer/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PhotoEnhancer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxResult = new System.Windows.Forms.PictureBox();
+            this.comboBoxFilters = new System.Windows.Forms.ComboBox();
+            this.buttonApply = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.BackColor = System.Drawing.Color.LemonChiffon;
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(232, 12);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(340, 255);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxOriginal.TabIndex = 0;
+            this.pictureBoxOriginal.TabStop = false;
+            // 
+            // pictureBoxResult
+            // 
+            this.pictureBoxResult.BackColor = System.Drawing.Color.LemonChiffon;
+            this.pictureBoxResult.Location = new System.Drawing.Point(232, 295);
+            this.pictureBoxResult.Name = "pictureBoxResult";
+            this.pictureBoxResult.Size = new System.Drawing.Size(340, 255);
+            this.pictureBoxResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxResult.TabIndex = 0;
+            this.pictureBoxResult.TabStop = false;
+            // 
+            // comboBoxFilters
+            // 
+            this.comboBoxFilters.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFilters.ForeColor = System.Drawing.SystemColors.Window;
+            this.comboBoxFilters.FormattingEnabled = true;
+            this.comboBoxFilters.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxFilters.Name = "comboBoxFilters";
+            this.comboBoxFilters.Size = new System.Drawing.Size(193, 21);
+            this.comboBoxFilters.TabIndex = 1;
+            this.comboBoxFilters.SelectedIndexChanged += new System.EventHandler(this.comboBoxFilters_SelectedIndexChanged);
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.buttonApply.Font = new System.Drawing.Font("Goudy Old Style", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonApply.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonApply.Location = new System.Drawing.Point(48, 510);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(99, 40);
+            this.buttonApply.TabIndex = 2;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = false;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 562);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.comboBoxFilters);
+            this.Controls.Add(this.pictureBoxResult);
+            this.Controls.Add(this.pictureBoxOriginal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "PhotoEnhancer v.0.1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.PictureBox pictureBoxResult;
+        private System.Windows.Forms.ComboBox comboBoxFilters;
+        private System.Windows.Forms.Button buttonApply;
+    }
+}
+
---
+++ PhotoEnhancer/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoEnhancer
+{
+    public partial class MainForm : Form
+    {
+        Panel parametersPanel;
+        Bitmap originalBMP;
+        Bitmap resultBMP; 
+        public MainForm()
+        {
+            InitializeComponent();
+
+            comboBoxFilters.Items.Add("Brighter | Darker");
+            comboBoxFilters.Items.Add("");
+
+
+            //pictureBoxOriginal.Image = Image.FromFile("Pear.jpg");
+            originalBMP=(Bitmap)Image.FromFile("Pear.jpg");
+            pictureBoxOriginal.Image = originalBMP;
+        }
+
+        private void comboBoxFilters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (parametersPanel != null)
+                this.Controls.Remove(parametersPanel);
+
+            parametersPanel = new Panel();
+
+            parametersPanel.Left = comboBoxFilters.Left;
+            parametersPanel.Width = comboBoxFilters.Width;
+            parametersPanel.Top = comboBoxFilters.Bottom+10;
+            parametersPanel.Height = buttonApply.Top - comboBoxFilters.Bottom - 20;
+            parametersPanel.BackColor = Color.BlanchedAlmond;
+
+
+            var filter = comboBoxFilters.SelectedItem;
+
+            if (filter.ToString()== "Brighter | Darker")
+            {
+                //MessageBox.Show("Brightening | darkening filter");
+                //parametersPanel.BackColor = Color.DarkSlateBlue;
+
+                var label = new Label();
+                label.Left = 0;
+                label.Top = 0;
+                label.Width = parametersPanel.Width - 50;
+                label.Height = 20;
+                label.Text = "coefficient";
+                parametersPanel.Controls.Add(label);
+
+                var inputBox = new NumericUpDown();
+                inputBox.Left = label.Right;
+                inputBox.Top = label.Top;
+                inputBox.Width = 50;
+                inputBox.Height = 20;
+                inputBox.Minimum = 0;
+                inputBox.Maximum = 10;
+                inputBox.Increment = (Decimal)0.05;
+                inputBox.DecimalPlaces = 2;
+                inputBox.Value = 1;
+                inputBox.Name = "coefficient";
+                parametersPanel.Controls.Add(inputBox);
+            }
+            else
+            {
+               // parametersPanel.BackColor=this.BackColor;
+            }
+            Controls.Add(parametersPanel);
+        }
+
+        private void buttonApply_Click(object sender, EventArgs e)
+        {
+            var newBMP = new Bitmap(originalBMP.Width, originalBMP.Height);
+            //using (var g = Graphics.FromImage(newBMP))
+            //{
+            //    g.DrawImage(resultBMP, new Rectangle(0, 0, newBMP.Width, newBMP.Height);
+
+                if(comboBoxFilters.SelectedItem.ToString() == "Brighter | Darker")
+                {
+                    for(int x=0; x<originalBMP.Width; x++)
+                        for (int y=0; y<originalBMP.Height; y++)
+                        {
+                            
+                        var pixelColor = originalBMP.GetPixel(x, y);
+
+                        var k = (double)((NumericUpDown)parametersPanel.Controls["coefficient"]).Value;
+
+                        var newR = (int)(pixelColor.R*k);
+                        var newG = (int)(pixelColor.G * k);
+                        var newB = (int)(pixelColor.B * k);
+                        var newColor = new Color();
+
+                        newBMP.SetPixel(x, y, Color.FromArgb(newR, newG, newB));
+
+                    }
+
+                }
+            //}
+
+
+            resultBMP = newBMP;
+            pictureBoxResult.Image = resultBMP;
+        }
+    }
+}
